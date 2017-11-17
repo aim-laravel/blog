5d6583c884b29dd14194c331a882af722b78da5c
Lakshay Verma <lakshay.verma@walkwel.in>
2017-11-17 06:13:30 +0530

    Use Yarn instead of NPM Run Watch Navigations updated to newer versions of Bulma

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,24 +1,14 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
 
 require('./bootstrap');
 
 window.Vue = require('vue');
-
 import Buefy from 'buefy'
 
 Vue.use(Buefy);
 
-// const app = new Vue({
-//     el: '#app'
+// Vue.component('example', require('./components/Example.vue'));
+
+// var app = new Vue({
+//   el: '#app',
+//   data: {}
 // });
-
-
-$(document).ready(function () {
-    $("button.dropdown").hover(function (e) {
-        $(this).toggleClass("is-open");
-    });
-});
--- resources/views/_includes/nav/main.blade.php
+++ resources/views/_includes/nav/main.blade.php
@@ -1,73 +1,62 @@
 <nav class="navbar has-shadow">
     <div class="container">
-        <div class="navbar-start">
-            <div class="navbar-brand">
-                <a class="navbar-item is-paddingless" href="{{route('home')}}">
-                    <img src="{{asset('images/devmarketer-logo.png')}}" alt="DevMarketer logo">
-                </a>
-            </div>
-            <a class="navbar-item is-tab is-hidden-mobile m-l-10">Learn</a>
-            <a class="navbar-item is-tab is-hidden-mobile">Discuss</a>
-            <a class="navbar-item is-tab is-hidden-mobile">Share</a>
-        </div>
-        <span class="navbar-toggle">
-            <span></span>
-            <span></span>
-            <span></span>
-        </span>
-        <div class="navbar-end" style="overflow: visible;">
-            <a class="navbar-item is-tab is-hidden-tablet is-active">Learn</a>
-            <a class="navbar-item is-tab is-hidden-tablet">Discuss</a>
-            <a class="navbar-item is-tab is-hidden-tablet">Share</a>
-            @guest
-            <a href="{{ route('login') }}" class="navbar-item is-tab">Login</a>
-            <a href="{{ route('register') }}" class="navbar-item is-tab">Join the Community</a>
-            @else
-            <button class="navbar-item is-tab dropdown-toggle">
-                <figure class="image is-16x16" style="margin-right: 8px;">
-                    <img src="http://bulma.io/images/jgthms.png">
-                </figure>
+        <div class="navbar-brand">
+            <a class="navbar-item is-paddingless" href="{{route('home')}}">
+                <img src="{{asset('images/devmarketer-logo.png')}}" alt="DevMarketer logo">
+            </a>
+            <button class="button navbar-burger">
+                <span></span>
+                <span></span>
+                <span></span>
             </button>
-            <button class="dropdown is-aligned-right navbar-item is-tab">
-                Hey {{ auth()->user()->name }}
-                <span class="icon">
-                    <i class="fa fa-caret-down"></i>
-                </span>
-                <ul class="dropdown-menu" style="overflow: visible;">
-                    <li>
-                        <a href="#">
+        </div>
+        <div class="navbar-menu">
+            <div class="navbar-start">
+                <a class="navbar-item is-tab is-active">Learn</a>
+                <a class="navbar-item is-tab">Discuss</a>
+                <a class="navbar-item is-tab">Share</a>
+            </div>
+            <!-- end of .navbar-start -->
+
+
+            <div class="navbar-end nav-menu" style="overflow: visible">
+                @guest
+                <a href="{{route('login')}}" class="navbar-item is-tab">Login</a>
+                <a href="{{route('register')}}" class="navbar-item is-tab">Join the Community</a>
+                @else
+                <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link">Hey {{Auth::user()->name}}</a>
+                    <div class="navbar-dropdown is-right">
+                        <a href="#" class="navbar-item">
                             <span class="icon">
                                 <i class="fa fa-fw fa-user-circle-o m-r-5"></i>
                             </span>Profile
                         </a>
-                    </li>
-                    <li>
-                        <a href="#">
+
+                        <a href="#" class="navbar-item">
                             <span class="icon">
                                 <i class="fa fa-fw fa-bell m-r-5"></i>
                             </span>Notifications
                         </a>
-                    </li>
-                    <li>
-                        <a href="{{ route('manage.dashboard') }}">
+                        <a href="{{route('manage.dashboard')}}" class="navbar-item">
                             <span class="icon">
                                 <i class="fa fa-fw fa-cog m-r-5"></i>
                             </span>Manage
                         </a>
-                    </li>
-                    <li class="seperator"></li>
-                    <li>
-                        <a href="{{route('logout')}}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                        <hr class="navbar-divider">
+                        <a href="{{route('logout')}}" class="navbar-item" onclick="event.preventDefault();
+                         document.getElementById('logout-form').submit();">
                             <span class="icon">
                                 <i class="fa fa-fw fa-sign-out m-r-5"></i>
                             </span>
                             Logout
                         </a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="post" style="display: none;">{{ csrf_field() }}</form>
-                    </li>
-                </ul>
-            </button>
-            @endif
+                        @include('_includes.forms.logout')
+                    </div>
+                </div>
+                @endguest
+            </div>
         </div>
+
     </div>
 </nav>
